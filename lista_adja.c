bc4b38c7ae6bff8d42b990c39039f982ad6e5128
caue <cauepaivalira@outlook.com>
2024-03-12 18:58:09 -0300

    mais trabalho nos grafos com lista de adjan

--- lista_adja.c
+++ lista_adja.c
@@ -7,15 +7,35 @@ struct no {
 };
 
 struct no_header { //header em cada index para apontar o seu número , o proximo grafo ligado e o grafo final da lista encad
-    int num_header;
+    uint num_header;
     struct no* prox;
     struct no* final;
 };
 
+struct grafo_la {
+     struct no_header** lista_headers;
+     uint num_nos;
+};
+
 typedef struct no No;
 typedef struct no_header NoHeader;
+typedef struct grafo_la GrafoLa;
+
+//codigo para os nos (lista encad) do grafo
+No* __grafos_la_final_lista(const No* no_comeco){
+    assert(no_comeco);
+    No* atual = no_comeco;
+
+    while(atual->prox){
+         atual = atual->prox;
+    }
+
+    return atual;
+}
 
 No* grafos_la_criar_no(const int numero) {
+    if (numero == 0)
+        err_exit("Numero do nos comecam a partir do 1, nao pode ser 0");
     No* no = (No*) malloc(sizeof(No));
     assert(no);
 
@@ -25,18 +45,45 @@ No* grafos_la_criar_no(const int numero) {
     return no;
 }
 
-NoHeader** grafos_la_criar_lista_headers(const int tam_lista){
+//codigo para os headers de cada no (acessar a lista encad)
+static NoHeader** __grafos_la_header_criar_lista(const int tam_lista){
+    if (tam_lista <= 0)
+        err_exit("Tam da lista n pode ser 0 ou negativo");
 
     NoHeader** lista_headers = (NoHeader**) malloc(sizeof(NoHeader*) * tam_lista);
     assert(lista_headers);
 
     for (int i = 0 ; i < tam_lista; i++){
+        lista_headers[i] = (NoHeader*) malloc(sizeof(NoHeader*));
+        assert(lista_headers[i]);
         lista_headers[i]->prox = NULL; //seta todos os headers com seus números e apontando pra null
         lista_headers[i]->final = NULL;
-        lista_headers[i]->num_header = i;
+        lista_headers[i]->num_header = i+1; //num dos nos começam do 1
     }
   
     return lista_headers;
+}
+
+static bool __grafos_la_header_adicionar_no(const int numero_header,No* no, NoHeader** lista_headers) {
+     assert_3ptrs(no,lista_headers,lista_headers[numero_header-1]);
+     if (numero_header <= 0)
+        err_exit("Numero do nos comecam a partir do 1, nao pode ser 0 ou negativo");
+     
+     NoHeader* header_atual = lista_headers[numero_header-1];
+
+     if (!header_atual->prox){ //se o NoHeader->prox estiver vazio
+         d_printf("header vazio");
+         header_atual->prox = no; 
+         header_atual->final = no; 
+         return true;
+     
+     }else{ //se o noheader estiver apontando para mais coisas
+          d_printf("header ja tem items");
+          No* final = __grafos_la_final_lista(header_atual->prox);
+          final->prox = no;
+          header_atual->final = no;
+          return true;
+     }
 }
 
 NoHeader** grafos_la_realocar_lista(NoHeader**lista_antiga, const int novo_tamanho) {
@@ -48,18 +95,75 @@ NoHeader** grafos_la_realocar_lista(NoHeader**lista_antiga, const int novo_taman
         return nova_lista;
 }
 
-bool grafos_la_adicionar_no(const int numero_header, const No* no, NoHeader** lista_headers) {
+static void __grafos_print_conexoes_no(const NoHeader* header){
+     assert(header);
+     No* atual = header->prox;
 
-     if (lista_headers[numero_header]) //se exister um valor
-        return false;
-    
+     while(atual){
+        printf("%d ",atual->numero_no);
+        atual = atual->prox;
+     }
+     printf("\n");
+}
 
+
+//codigo para o TAD grafos em si (que o user vai acessar)
+GrafoLa* grafo_la_criar(const uint numero_nos){
+    if (numero_nos < 0)
+        err_exit("num de nos n pode ser negativo");
+
+    GrafoLa* grafo = (GrafoLa*) malloc(sizeof(GrafoLa));
+    assert(grafo);
+
+    grafo->num_nos = numero_nos;
+    grafo->lista_headers = __grafos_la_header_criar_lista(numero_nos);
+
+    return grafo;
+}
+
+bool grafo_la_adicionar_conexao(const int numero_no, No* nova_conexao, GrafoLa* grafo){
+    if (numero_no == 0)
+        err_exit("Numero do nos comecam a partir do 1, nao pode ser 0");
+
+    if (numero_no > grafo->num_nos){
+        err_exit("No para add conexao %d nao existe no grafo com %d nos",numero_no,  grafo->num_nos);
+    }
+    if (nova_conexao->numero_no > grafo->num_nos){
+        err_exit("No da nova conexao %d nao existe no grafo com %d nos", nova_conexao->numero_no,  grafo->num_nos);        
+    }
+    return __grafos_la_header_adicionar_no(numero_no,nova_conexao,grafo->lista_headers);
+}
+
+
+//funcoes de i/o para o usuário
+void grafos_la_print(const GrafoLa* grafo){
+     
+    NoHeader** lista_headers = grafo->lista_headers;
+    uint numero_nos = grafo->num_nos;
+
+    for (int i = 0; i < numero_nos ; i++){
+         printf("No: %d ,Conexoes: ", lista_headers[i]->num_header);
+         __grafos_print_conexoes_no(lista_headers[i]);
+        
+    }
 }
 
 
 int main(){
 
-    NoHeader** lista_grafos = criar_listas_grafos(3);
+    GrafoLa* grafo = grafo_la_criar(6);
+    No* no1 = grafos_la_criar_no(3);
+    No* no2 = grafos_la_criar_no(5);
+
+    grafo_la_adicionar_conexao(1,no1,grafo);
+    grafo_la_adicionar_conexao(1,no2,grafo);
+    grafo_la_adicionar_conexao(6,no2,grafo);
+
+    grafos_la_print(grafo);
+
+    //printf("%d %d \n", lista_grafos[0]->prox->numero_no, lista_grafos[0]->final->numero_no );
+
+    /*NoHeader** lista_grafos = criar_listas_grafos(3);
 
     No* no1 = criar_no(2);
     No* no2 = criar_no(3);
@@ -67,5 +171,5 @@ int main(){
     lista_grafos[0] = no1;
     lista_grafos[0]->prox = no2;
 
-    printf("%d \n", lista_grafos[0]->prox->numero_no);
+    printf("%d \n", lista_grafos[0]->prox->numero_no);*/
 }
--- lista_adja.h
+++ lista_adja.h
@@ -6,5 +6,80 @@
    #include "assert.h"
    #include "stdbool.h"
 
+    #define assert_2ptrs(p1,p2) assert(p1); assert(p2);
+    #define assert_3ptrs(p1,p2,p3) assert_2ptrs(p1,p2); assert(p3);
+
+    //simple max and min macro
+    #define max(x, y)  ((x) > (y) ? (x) : (y))
+
+    #define min(x, y)  ((x) > (y) ? (y) : (x))
+
+    //frees the pointer pointed by the double pointer and sets it to NULL, this CAN be used on a ptr passed to a func
+    #define safe_free_double(double_ptr) do {\
+      free(*(double_ptr));\
+      *(double_ptr) = NULL;\
+    } while(0)
+    
+    //frees a pointer and sets it to NULL, this CANT be used on a ptr passed to a func
+    #define safe_free(ptr) do {\
+      free((ptr));\
+      (ptr) = NULL;\
+    } while(0)
+    
+    // if a function  returns -1, this raises an error and prints the errno messages, this is useful for stdlib functions that change
+    // errno val
+    #define errno_check(X) do {\
+        if ((X) == -1) {\
+            printf("ERROR: (VAR NAME: %s , FILE: %s , LINE: %d) -- Error name: %s \n", #X ,__FILE__,__LINE__, strerror(errno));\
+            exit(1);\
+        }\
+        } while (0)
+        //strerror comes from string.h and prints the messages from the errno error
+
+
+    //nullptr and errno check macro
+    #define null_errno_check(ptr) do {\
+        void* __null_errno_ptr = (ptr);\
+        if((__null_errno_ptr) == NULL){\
+           printf("NULL PTR ERROR: (VAR NAME: %s , FILE: %s , LINE: %d) -- Error name: %s \n", #ptr ,__FILE__,__LINE__, strerror(errno));\
+           exit(1);\
+        }\
+    } while(0)
+
+    #define err_exit(...) do {\
+            printf("[ERROR] (FILE: %s , LINE: %d) -- ",__FILE__,__LINE__);\
+            printf(__VA_ARGS__);\
+            printf("\nExit(1)\n");\
+            exit(1);\
+    } while(0)
+    //used to check for null ptrs and displaying errno messages, useful for file opening routines
+
+
+    //get the size of an array
+    #define arr_size(arr) (sizeof( (arr) ) / sizeof((arr)[0]))
+
+
+    // debug macros
+    #define DEBUG 1 //debug or not flag
+    #if DEBUG //printf with more debug info
+        #define d_printf(...) do {\
+            printf("[DEBUG] (FILE: %s , LINE: %d) -- ",__FILE__,__LINE__);\
+            printf(__VA_ARGS__);\
+            printf("\n");\
+        } while(0)
+    #else    //does not print anything
+        #define d_printf(...) (void)0
+    #endif  
+
+    //single line debug printf macro, its reliant on a GCC ##__VA_ARGS__ variable so it might be less portable, it can also work if d_printf is called with no args
+    //#define d_printf2(format, ...) printf("[DEBUG] (FILE: %s , LINE: %d) -- " format "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+
+
+
+    //ATTENTION: warning arg needs to be a string
+    #define warn_printf(warning) printf("[WARNING] %s (FILE: %s , LINE: %d) -- \n", (warning) ,__FILE__,__LINE__);\
+
+    typedef unsigned int uint;
+
 
 #endif
