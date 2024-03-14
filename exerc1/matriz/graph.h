f80ca7b786a03392930984ba21636f3dd7aa6b25
caue <cauepaivalira@outlook.com>
2024-03-13 22:26:49 -0300

    exerc1

--- exerc1/matriz/graph.h
+++ exerc1/matriz/graph.h
@@ -10,12 +10,8 @@
     #define assert_2ptrs(p1,p2) assert(p1); assert(p2);
     #define assert_3ptrs(p1,p2,p3) assert_2ptrs(p1,p2); assert(p3);
 
-    
     #define max(x, y)  ((x) > (y) ? (x) : (y))
-
     #define min(x, y)  ((x) > (y) ? (y) : (x))
-
-
 
     #define err_exit(...) do {\
             printf("[ERROR] (FILE: %s , LINE: %d) -- ",__FILE__,__LINE__);\
@@ -30,12 +26,10 @@
             printf("\n");\
     } while(0)
    
-
     #define warn_printf(warning) printf("[WARNING] %s (FILE: %s , LINE: %d) -- \n", (warning) ,__FILE__,__LINE__);\
 
-
     #define NO_EDGE -1 //define para representar a não existencia de uma aresta na matriz de adjacencia com pesos
-    #define VERTEX_LIST_CHUNK 2 //define de quantos em quantos vertices vamos alocar para a lista de vertices
+    #define VERTEX_LIST_CHUNK 20 //define de quantos em quantos vertices vamos alocar para a lista de vertices
 
     typedef struct graph Graph;
 
